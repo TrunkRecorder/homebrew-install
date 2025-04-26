5428a1037277bf2dfeb8d072fdc24a9e23158233
Nick M <taclane@users.noreply.github.com>
2025-04-26 09:05:04 -0400

    5.0.2 update

--- Formula/trunk-recorder.rb
+++ Formula/trunk-recorder.rb
@@ -1,19 +1,10 @@
 class TrunkRecorder < Formula
   desc "Trunked & Conventional Radio Recorder"
   homepage "https://github.com/TrunkRecorder/trunk-recorder"
+  url "https://github.com/TrunkRecorder/trunk-recorder/archive/refs/tags/v5.0.2.tar.gz"
+  sha256 "a153cf3b16b81e13e8bdb151a0c495d19a8fd46d63d8e76d9bc7750c2b0c7820"
   license "GPL-3.0-or-later"
   head "https://github.com/TrunkRecorder/trunk-recorder.git", branch: "master"
-
-  stable do
-    url "https://github.com/TrunkRecorder/trunk-recorder/archive/refs/tags/v5.0.1.tar.gz"
-    sha256 "1c9a8a625506c5b5cc14948404e70b86333ea9cd08c0cfef117f611497d5b2be"
-    # A patch is required to build with updated Boost libraries provided by Homebrew.
-    # Remove on next tagged release; these fixes are already present in the master branch.
-    patch do
-      url "https://raw.githubusercontent.com/TrunkRecorder/homebrew-install/1819dfd30a659f9b5b061b3cc27aeebf34a31469/Patches/trunk-recorder-5.0.1.diff"
-      sha256 "db20b1e0ac0534bc4fd189be93b6cf67d6bdd188ba8907e49c960760801df586"
-    end
-  end
 
   depends_on "cmake" => :build
   depends_on "cppunit" => :build
